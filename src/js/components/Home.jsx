dad58a1b3190ffbcf92c5c9dc35c7d4cab5215ab
David Rosas <156865176+davidrosas809@users.noreply.github.com>
2025-01-04 03:59:56 +0000

    Completed

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-
+import { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ color, colorset ] = useState ("green");
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="bg-dark mx-auto m-0 p-0" style={{width:"10px", height:"180px"}}>
+				z
+			</div>
+			<div className="bg-dark mx-auto p-3 rounded-4 "style={{width:"230px", height:"auto"}}>.
+				<div className={"bg-danger m-2 mx-auto rounded-circle "+(color=="red"? "brillo":" ")}
+				onClick={() => colorset("red")}
+				style={{width:"180px", height:"180px"}}>.</div>
+				<div className={"bg-warning m-2 mx-auto rounded-circle "+(color=="yellow"? "brillo":" ")} 
+				onClick={() => colorset("yellow")}
+				style={{width:"180px", height:"180px"}}>.</div>
+				<div className={"bg-success m-2 mx-auto rounded-circle "+(color=="green"? "brillo":" ")} 
+				onClick={() => colorset("green")}
+				style={{width:"180px", height:"180px"}}>.</div>
+			</div>			
 		</div>
 	);
 };
